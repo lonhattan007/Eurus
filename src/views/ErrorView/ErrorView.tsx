5137e04bce7144fcd6afa78f9524a342b6d4cfc7
Nhat Tan <66843927+lonhattan007@users.noreply.github.com>
2023-05-30 14:59:15 +0700

    Updated documents

--- src/views/ErrorView/ErrorView.tsx
+++ src/views/ErrorView/ErrorView.tsx
@@ -12,9 +12,9 @@ const ErrorView = () => {
 
   return (
     <div className='flex flex-col justify-center items-center p-0 m-auto'>
-      <h1 className='m-3'>Oops!</h1>
+      <h1 className='m-3 font-bold text-[3.2em]'>Oops!</h1>
       <h3 className='m-3'>Sorry, an unexpected error has occurred.</h3>
-      <p className='italic'>
+      <p className='italic font-bold'>
         {`${error.status} - ${error.statusText || error.message}`}
       </p>
     </div>
